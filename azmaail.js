54a204ba903760a0091e775669f13e9f23219f70
SK AZMAAIL <79507492+poetazmaail@users.noreply.github.com>
2022-04-10 23:09:07 +0530

    Add files via upload

---
+++ azmaail.js
@@ -0,0 +1,19 @@
+let x = prompt('Enter first variable:');
+let y = prompt('Enter second variable:');
+
+let third;
+
+third = x;
+x = y;
+y = third;
+
+console.log('The value of x after swapping: {$x}');
+console.log('The value of y after swapping: {$b');
+
+/*     OUTPUT
+Enter first variable: 10
+Enter second variable: 15
+The value of x after swapping: 15
+The value of y after swapping: 10
+
+                                              */
